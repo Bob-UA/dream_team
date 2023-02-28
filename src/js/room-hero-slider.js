59222242fb97b9ada44cc10cfe1dccc70dfe26e9
ZorAnderius <gabriel.zorander@gmail.com>
2023-02-28 22:51:31 +0000

    add some files

--- src/js/room-hero-slider.js
+++ src/js/room-hero-slider.js
@@ -1,9 +1,9 @@
 function prev() {
-  document.getElementById('room-slider-container').scrollLeft -= 270;
+  document.getElementById('slider-container').scrollLeft -= 270;
 }
 
 function next() {
-  document.getElementById('room-slider-container').scrollLeft += 270;
+  document.getElementById('lider-container').scrollLeft += 270;
 }
 
 $('.room-img-item img').on('click', function () {

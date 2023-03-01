9be94c88f9b86602183d97d3312ce308b75d6cf8
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-02-28 17:35:23 -0800

    fixed

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-import './js/restourants-details-modal';
+// import './js/restourants-details-modal';
 
-import './js/slider-restaurant-page.js';
+// import './js/slider-restaurant-page.js';
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+
+function prev() {
+  document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next() {
+  document.getElementById('slider-container').scrollLeft += 270;
+}
+
+$('.restaurant-img-item img').on('click', function () {
+  $(this).toggleClass('zoomed');
+  $('.overlay').toggleClass('active');
+});

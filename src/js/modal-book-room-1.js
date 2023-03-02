3303e29d409e3fe1fb61d7b0044278b128049763
alexandr-ni <sashanikitenko119@gmail.com>
2023-03-02 14:14:58 +0200

    bugfix with opening modal in mobile-tablet version

--- src/js/modal-book-room-1.js
+++ src/js/modal-book-room-1.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-room-open-first]'),
+    openModalBtn1: document.querySelector('[data-modal-room-open-first1]'),
     closeModalBtn: document.querySelector('[data-modal-room-close-first]'),
     modal: document.querySelector('[data-modal-room-first]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {

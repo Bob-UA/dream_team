e2190a1b7dc34a0ed43d1f84e29e905063479aa8
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-03-02 03:44:22 -0800

    changed main js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
-import './js/modal-book-room';
+import * as utils from './js/modal-book-room';
 
-import './js/modal-book-room-1';
+import * as utils from './js/modal-book-room-1';
 
-import './js/modal-book-room-2';
+import * as utils from './js/modal-book-room-2';
 
-import './js/modal-book-room-3';
+import * as utils from './js/modal-book-room-3';
 
-import './js/modal-book-table';
+import * as utils from './js/modal-book-table';
 
-import './js/modal-book-table-1';
+import * as utils from './js/modal-book-table-1';
 
-import './js/modal-book-table-2';
+import * as utils from './js/modal-book-table-2';
 
-import './js/modal-book-table-3';
+import * as utils from './js/modal-book-table-3';
 
-import './js/modal-price';
+import * as utils from './js/modal-price';
 
-import './js/modal-price-1';
+import * as utils from './js/modal-price-1';
 
-import './js/modal-price-2';
+import * as utils from './js/modal-price-2';
 
-import './js/modal-price-3';
+import * as utils from './js/modal-price-3';
 
-import './js/modal-price-4';
+import * as utils from './js/modal-price-4';
 
-import './js/room-hero-slider.js';
+import * as utils from './js/room-hero-slider.js';
 
 import './js/slider-restaurant-page.js';
 

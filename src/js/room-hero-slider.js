1e2823cf719d5a386f7ae2a6b34a419e0f51c4ed
ZorAnderius <gabriel.zorander@gmail.com>
2023-03-02 15:40:06 +0000

    upgrade slider

--- src/js/room-hero-slider.js
+++ src/js/room-hero-slider.js
@@ -5,8 +5,3 @@ function prev() {
 function next() {
   document.getElementById('room-slider-container').scrollLeft += 270;
 }
-
-$('.room-img-item img').on('click', function () {
-  $(this).toggleClass('zoomed');
-  $('.overlay').toggleClass('active');
-});

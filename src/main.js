92c61daedd765ec3dd480c69313a9bb5b9945bbc
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-03-02 04:26:22 -0800

    q

--- src/main.js
+++ src/main.js
@@ -1,33 +1,3 @@
-import * as utils from './js/modal-book-room';
-
-import * as utils from './js/modal-book-room-1';
-
-import * as utils from './js/modal-book-room-2';
-
-import * as utils from './js/modal-book-room-3';
-
-import './js/modal-book-room-4';
-
-import './js/modal-book-table';
-
-import * as utils from './js/modal-book-table-1';
-
-import * as utils from './js/modal-book-table-2';
-
-import * as utils from './js/modal-book-table-3';
-
-import * as utils from './js/modal-price';
-
-import * as utils from './js/modal-price-1';
-
-import * as utils from './js/modal-price-2';
-
-import * as utils from './js/modal-price-3';
-
-import * as utils from './js/modal-price-4';
-
-import * as utils from './js/room-hero-slider.js';
-
 import './js/slider-restaurant-page.js';
 
 import './js/menu.js';
@@ -36,3 +6,38 @@ import './js/header-hoover.js';
 
 import './js/current';
 
+import './js/modal-book-room';
+
+import './js/modal-book-room-1';
+
+import './js/modal-book-room-2';
+
+import './js/modal-book-room-3';
+
+import './js/modal-book-room-4';
+
+import './js/modal-book-table';
+
+import './js/modal-book-table-1';
+
+import './js/modal-book-table-2';
+
+import './js/modal-book-table-3';
+
+import './js/modal-price';
+
+import './js/modal-price-1';
+
+import './js/modal-price-2';
+
+import './js/modal-price-3';
+
+import './js/modal-price-4';
+
+import './js/room-hero-slider.js';
+
+import './js/slider-restaurant-page.js';
+
+import './js/menu.js';
+
+import './js/header-hoover.js';

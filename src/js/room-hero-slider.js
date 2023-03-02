7d26a9584fc1cb30325e4319db5adbeb2379d6df
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-03-02 08:03:57 -0800

    q

--- src/js/room-hero-slider.js
+++
@@ -1,7 +0,0 @@
-function prev() {
-  document.getElementById('room-slider-container').scrollLeft -= 270;
-}
-
-function next() {
-  document.getElementById('room-slider-container').scrollLeft += 270;
-}

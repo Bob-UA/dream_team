2a1694b009cf912ae6081312c38cefdd8623ceee
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-10-17 13:37:28 -0700

    changed styles for buttons

--- src/js/hotel-hero-js.js
+++ src/js/hotel-hero-js.js
@@ -1,13 +1,15 @@
+const nextBtn = document.querySelector('.slider-btn-first');
+console.log(nextBtn);
+
 function prev() {
-  document.getElementById('slider-container').scrollLeft -= 270;
+  document.getElementById('hotel-slider-container').scrollLeft -= 270;
 }
 
 function next() {
-  document.getElementById('slider-container').scrollLeft += 270;
+  document.getElementById('hotel-slider-container').scrollLeft += 270;
 }
 
 $('.hotel-img-item img').on('click', function () {
   $(this).toggleClass('zoomed');
   $('.overlay').toggleClass('active');
 });
-
